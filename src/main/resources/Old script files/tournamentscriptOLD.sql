6b4a31d08ae51ea102576d9e80f7a1d55cdb6a64
Morten Bendeke <mbendeke@hotmail.com>
2022-09-09 10:39:28 +0200

    new sql script

--- src/main/resources/Old script files/tournamentscriptOLD.sql
+++ src/main/resources/Old script files/tournamentscriptOLD.sql
@@ -5,17 +5,13 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema Tournament
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `Tournament` ;
 
 -- -----------------------------------------------------
 -- Schema Tournament
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Tournament` DEFAULT CHARACTER SET utf8mb3 ;
+CREATE SCHEMA IF NOT EXISTS `Tournament` DEFAULT CHARACTER SET utf8 ;
 USE `Tournament` ;
 
 -- -----------------------------------------------------
@@ -25,32 +21,11 @@ DROP TABLE IF EXISTS `Tournament`.`Tournament` ;
 
 CREATE TABLE IF NOT EXISTS `Tournament`.`Tournament` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `registration_date` DATE NOT NULL,
-  `game_type` VARCHAR(45) NOT NULL,
-  `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` VARCHAR(45) NULL,
+  `registration_date` DATE NULL,
+  `game_type` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `Tournament`.`Match`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Tournament`.`Match` ;
-
-CREATE TABLE IF NOT EXISTS `Tournament`.`Match` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATE NULL DEFAULT NULL,
-  `Tournament_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Match_Tournament1_idx` (`Tournament_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Match_Tournament1`
-    FOREIGN KEY (`Tournament_id`)
-    REFERENCES `Tournament`.`Tournament` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -60,10 +35,28 @@ DROP TABLE IF EXISTS `Tournament`.`Team` ;
 
 CREATE TABLE IF NOT EXISTS `Tournament`.`Team` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Tournament`.`Match`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Tournament`.`Match` ;
+
+CREATE TABLE IF NOT EXISTS `Tournament`.`Match` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NULL,
+  `Tournament_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Match_Tournament1_idx` (`Tournament_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Match_Tournament1`
+    FOREIGN KEY (`Tournament_id`)
+    REFERENCES `Tournament`.`Tournament` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -79,12 +72,15 @@ CREATE TABLE IF NOT EXISTS `Tournament`.`Match_has_Team` (
   INDEX `fk_Match_has_Team_Match1_idx` (`Match_id` ASC) VISIBLE,
   CONSTRAINT `fk_Match_has_Team_Match1`
     FOREIGN KEY (`Match_id`)
-    REFERENCES `Tournament`.`Match` (`id`),
+    REFERENCES `Tournament`.`Match` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Match_has_Team_Team1`
     FOREIGN KEY (`Team_id`)
-    REFERENCES `Tournament`.`Team` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `Tournament`.`Team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -98,14 +94,17 @@ CREATE TABLE IF NOT EXISTS `Tournament`.`Tournament_has_Team` (
   PRIMARY KEY (`Tournament_id`, `Team_id`),
   INDEX `fk_Tournament_has_Team_Team1_idx` (`Team_id` ASC) VISIBLE,
   INDEX `fk_Tournament_has_Team_Tournament1_idx` (`Tournament_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Tournament_has_Team_Team1`
-    FOREIGN KEY (`Team_id`)
-    REFERENCES `Tournament`.`Team` (`id`),
   CONSTRAINT `fk_Tournament_has_Team_Tournament1`
     FOREIGN KEY (`Tournament_id`)
-    REFERENCES `Tournament`.`Tournament` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `Tournament`.`Tournament` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Tournament_has_Team_Team1`
+    FOREIGN KEY (`Team_id`)
+    REFERENCES `Tournament`.`Team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
